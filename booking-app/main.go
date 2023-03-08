302223cef09686ef12aa36b2432613c1988f76c3
hiromu-kon <hiro.0405soccer@icloud.com>
2023-03-08 22:21:47 +0900

    fix

--- booking-app/main.go
+++ booking-app/main.go
@@ -6,6 +6,11 @@ func main() {
 	fmt.Println("Hello World")
 
 	var conferenceName = "Go Conference"
+	const conferenceTickets = 50
 	conferenceName = "Change ConferenceName"
-	fmt.Println(conferenceName)
+	fmt.Println("Hello", conferenceName)
+	fmt.Println(conferenceTickets)
+
+	var username string = "Tom"
+	fmt.Println(username)
 }
